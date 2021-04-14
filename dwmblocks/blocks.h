68ced9da421d3aa27eb767e83e1a5893251b7cae
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2021-04-14 21:08:57 +0530

    make dwmblocks clickable

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -4,10 +4,13 @@
 
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", PATH("sb-clock"), 1, 1},
+	/*Icon*/	/*Command*/             /*Update Interval*/	        /*Update Signal*/
+    {"",        "echo ' '",                 0,                              0},
+    {"",        PATH("sb-clock"),           1,                              1},
+    {"",        PATH("sb-memory"),          2,                              2},
+    {"",        "echo ' '",                 0,                              0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
