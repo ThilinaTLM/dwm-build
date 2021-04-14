60830982d3de9a1db5a2a65ea2bdc2cadfe9539b
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2021-04-14 14:39:48 +0530

    add always center patch

--- src/dwm.c
+++ src/dwm.c
@@ -1064,6 +1064,11 @@ manage(Window w, XWindowAttributes *wa)
 	updatewindowtype(c);
 	updatesizehints(c);
 	updatewmhints(c);
+
+    // make floating window alwayscenter
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+	c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
+
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
 	if (!c->isfloating)
